d492638dbad444975707d5fd625adffe47175cf1
Bisarga Panda <bisargpanda@gmail.com>
2016-08-08 13:46:42 +0530

    Add files via upload

---
+++ prob1.py
@@ -0,0 +1,8 @@
+def max(n1,n2):
+    if(n1 > n2):
+        return n1
+    else:
+        return n2
+num1 = input("Enter n1 : ")
+num2 = input("enter n2 : ")
+print "Max number is :" + str(max(num1,num2))
